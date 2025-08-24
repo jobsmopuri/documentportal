d7c3715349610080da4afbe2715ed745885b38aa
Sivakumar Mopuri <jobs.mopuri@gmail.com>
2025-08-24 21:37:12 +0530

    WIP

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -5,3 +5,22 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocuementPortalException
 
 class DocumentComparator:
+    def __init__(self):
+        pass
+    def delete_existing_file(self):
+        """
+        Deleting existing files at the specific path
+        """
+        pass
+    def save_uploded_files(self):
+        """
+        Saves uploaded files to a specific directory
+        """
+        pass
+
+    def read_pdf(self):
+        """
+        reads a PDF file and extract the text from page.
+        """
+        pass
+
